a72461c21ab46a8c0cb3f55221acb74ee7d091ea
jinghao <jinghaoluo@infinite.net.cn>
2024-09-11 11:57:51 +0800

    perf: footer

--- pages/layout/footer.tsx
+++ pages/layout/footer.tsx
@@ -3,7 +3,7 @@ import { footerClass } from "../styles"
 const Footer = () => {
   return (
     <footer className={footerClass}>
-      <p>© {new Date().getFullYear()} <a href="/">{Bun.env.TITLE}</a>. Powered by <a href="/" target="_blank">Quest</a> &amp; <a href="https://www.yige.me" target="_blank">YIGE</a>.</p>
+      <p>© {new Date().getFullYear()} <a href="/">{Bun.env.TITLE}</a>. Powered by <a href="https://github.com/jakholuo/quest" target="_blank">Quest</a> &amp; <a href="https://www.yige.me" target="_blank">YIGE</a>.</p>
     </footer>
   )
 }
