46656fd0f9a92333fff678d78fd208c6db512176
jinghao <jinghaoluo@infinite.net.cn>
2024-09-12 15:03:04 +0800

    feat: add a href target _blank

--- utils/marked.ts
+++ utils/marked.ts
@@ -1,6 +1,7 @@
 import { marked } from 'marked'
 
-const renderer = new marked.Renderer();
+const renderer = new marked.Renderer()
+
 renderer.link = function(href: string, title: string, text: string) {
   var link = marked.Renderer.prototype.link.apply(this, arguments)
   return link.replace("<a","<a target='_blank'")
